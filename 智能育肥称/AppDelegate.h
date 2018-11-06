881d14efebb5f42ad99bb05d01f2b04e8ae42b57
mengwenlong <1730717923@qq.com>
2018-11-06 09:12:16 +0800

    Initial Commit

---
+++ 智能育肥称/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  智能育肥称
+//
+//  Created by mco on 2018/11/6.
+//  Copyright © 2018年 mco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
